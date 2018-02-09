9a817f0773a7d51ce21c3ef901984c57b9d238f3
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-09 02:55:31 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,11 +1,10 @@
 module ArtistsHelper
 
-  def display_artist(artist)
+  def display_artist
     if artist_name
       link_to artist_path(artist)
     else
       link_to "Add Artist", edit_song_path(artist.song)
-      #If the artist name is nil, return a link to the song's edit page, with a link text of "Add Artist"
     end
   end
 end
