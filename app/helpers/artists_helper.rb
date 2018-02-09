3007bfdb969094f46b23c9ab5dbc697e74221f9b
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-09 03:16:13 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    if artist_name
+      link_to artist_path(song.artist)
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
+
 end
